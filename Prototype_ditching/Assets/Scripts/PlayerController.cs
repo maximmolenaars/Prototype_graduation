03fb9712ed27b0488fb9c5a5e968960c0d59ad49
Maxim Molenaars <maximmolenaars@gmail.com>
2018-09-12 16:58:26 +0200

    Move door on mouseClick

--- Prototype_ditching/Assets/Scripts/PlayerController.cs
+++ Prototype_ditching/Assets/Scripts/PlayerController.cs
@@ -1,23 +1,49 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 using UnityEngine.EventSystems;
 
-public class PlayerController : MonoBehaviour {
+public class PlayerController : MonoBehaviour
+{
 
     public float rayLength;
     public LayerMask layermask;
+    public GameObject doorAssist;
+    public GameObject door14;
 
-    private void Update()
+    private void Start()
     {
-        if (Input.GetMouseButtonDown(0) && !EventSystem.current.IsPointerOverGameObject()) 
+        doorAssist = GameObject.Find("/Door_14/Friendly");
+        door14 = GameObject.Find("Door_14");
+        Debug.Log("hello there");
+    }
+
+
+    public void Update()
+    {
+        if (Input.GetMouseButtonDown(0) && !EventSystem.current.IsPointerOverGameObject())
         {
             RaycastHit hit;
             Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
             if (Physics.Raycast(ray, out hit, rayLength, layermask))
             {
                 Debug.Log(hit.collider.name);
+                if (hit.collider.name == "Friendly")
+                {
+                    Debug.Log("Something was clicked!");
+                    OpenDoor();
+                }
+
             }
         }
+
+
     }
 
-
+    private void OpenDoor()
+    {
+        door14.transform.position = new Vector3(0.0f, 1.5f, 0.0f);
+    }
 }
+
+
+
