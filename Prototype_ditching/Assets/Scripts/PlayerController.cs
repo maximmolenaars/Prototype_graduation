23d4636471d3109081365def5c0d3dc586dc8d3d
Maxim Molenaars <maximmolenaars@gmail.com>
2018-09-13 11:10:41 +0200

    Added plane body and slide spawn function

--- Prototype_ditching/Assets/Scripts/PlayerController.cs
+++ Prototype_ditching/Assets/Scripts/PlayerController.cs
@@ -9,11 +9,12 @@ public class PlayerController : MonoBehaviour
     public LayerMask layermask;
     public GameObject doorAssist;
     public GameObject door14;
+    public GameObject slideToSpawn;
 
     private void Start()
     {
-        doorAssist = GameObject.Find("/Door_14/Friendly");
-        door14 = GameObject.Find("Door_14");
+        doorAssist = GameObject.Find("/BoeingInterior/BP_AirplaneDoor2");
+        door14 = GameObject.Find("/BoeingInterior/BP_AirplaneDoor2");
         Debug.Log("hello there");
     }
 
@@ -27,7 +28,7 @@ public class PlayerController : MonoBehaviour
             if (Physics.Raycast(ray, out hit, rayLength, layermask))
             {
                 Debug.Log(hit.collider.name);
-                if (hit.collider.name == "Friendly")
+                if (hit.collider.name == "BP_AirplaneDoor2")
                 {
                     Debug.Log("Something was clicked!");
                     OpenDoor();
@@ -41,7 +42,18 @@ public class PlayerController : MonoBehaviour
 
     private void OpenDoor()
     {
-        door14.transform.position = new Vector3(0.0f, 1.5f, 0.0f);
+        door14.transform.position = new Vector3(-3.08f, 1.28f, 1.258f);
+        SpawnSlide();
+    }
+
+    private void SpawnSlide()
+    {
+        Debug.Log("Slide is spawned");
+        GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+        cube.transform.localScale += new Vector3(5f, -0.1f, 2f);
+        cube.AddComponent<Rigidbody>();
+        cube.transform.position = new Vector3(-5.83f, 0.2293683f, -0.1403087f);
+
     }
 }
 
